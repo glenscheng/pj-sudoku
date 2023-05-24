7cd36b0864bb4eff9bf05cfa9be3505340f1402b
Cheng <gcheng46@elsys.gtri.org>
2023-05-24 15:40:43 -0400

    still adding improvements

--- sudoku_improved/sudoku_improved.js
+++ sudoku_improved/sudoku_improved.js
@@ -1,5 +1,14 @@
-var numSelected = null;
+var digitSelected = null;
 var tileSelected = null;
+var one = 4;
+var two = 3;
+var three = 3;
+var four = 4;
+var five = 3;
+var six = 5;
+var seven = 5;
+var eight = 4;
+var nine = 4;
 
 var board = [
     "--74916-5",
@@ -57,7 +66,7 @@ function setGame() {
         let digitsTile = document.createElement("div"); // create a div
         digitsTile.id = i; // set id
         digitsTile.innerText = i; // set digit
-        digitsTile.addEventListener("click", selectNumber); // detect click
+        digitsTile.addEventListener("click", selectDigit); // detect click
         digitsTile.classList.add("digits-tile"); // add formatting
         document.getElementById("digits").appendChild(digitsTile);
     }
@@ -78,24 +87,250 @@ function selectTile() {
     }
 }
 
-function selectNumber() {
+function selectDigit() {
     if (tileSelected != null) { // digit selection
         if (tileSelected.classList.contains("tile-start") == true) { // return if already number at that boardTile
             return;
         }
 
-        let coords = tileSelected.id.split("-"); // ["0", "0"] ...
-        let row = parseInt(coords[0]);
-        let col = parseInt(coords[1]);
-        tileSelected.innerText = this.id; // place down selected digit IF IT IS CORRECT
+        // let coords = tileSelected.id.split("-"); // ["0", "0"] ...
+        // let row = parseInt(coords[0]);
+        // let col = parseInt(coords[1]);
+        let oldDigitSelected = digitSelected;
+        digitSelected = this;
+
+        let oldTileSelectedInnerText = tileSelected.innerText;
+        /*if (checkToStopPlacingDigits == true) {
+            return;
+        }*/
+        switch(digitSelected.id) {
+            case "1":
+                if (one == 9) {
+                    return;
+                }
+                break;
+            case "2":
+                if (two == 9) {
+                    return;
+                }
+                break;
+            case "3":
+                if (three == 9) {
+                    return;
+                }
+                break;
+            case "4":
+                if (four == 9) {
+                    return;
+                }
+                break;
+            case "5":
+                if (five == 9) {
+                    return;
+                }
+                break;
+            case "6":
+                if (six == 9) {
+                    return;
+                }
+                break;
+            case "7":
+                if (seven == 9) {
+                    return;
+                }
+                break;
+            case "8":
+                if (eight == 9) {
+                    return;
+                }
+                break;
+            case "9":
+                if (nine == 9) {
+                    return;
+                }
+                break;
+        }
+        tileSelected.innerText = digitSelected.id; // place down selected digit
+        if (oldDigitSelected != null && oldTileSelectedInnerText != "" &&
+            tileSelected.innerText != oldTileSelectedInnerText) {
+            switch(oldTileSelectedInnerText) {
+                case "1":
+                    one--; break;
+                case "2":
+                    two--; break;
+                case "3":
+                    three--; break;
+                case "4":
+                    four--; break;
+                case "5":
+                    five--; break;
+                case "6":
+                    six--; break;
+                case "7":
+                    seven--; break;
+                case "8":
+                    eight--; break;
+                case "9":
+                    nine--; break;
+            }
+        }
+        if (/*tileSelected.innerText == "" ||*/ tileSelected.innerText != oldTileSelectedInnerText) {
+            switch(digitSelected.id) {
+                case "1":
+                    one++; break;
+                case "2":
+                    two++; break;
+                case "3":
+                    three++; break;
+                case "4":
+                    four++; break;
+                case "5":
+                    five++; break;
+                case "6":
+                    six++; break;
+                case "7":
+                    seven++; break;
+                case "8":
+                    eight++; break;
+                case "9":
+                    nine++; break;
+            }
+        }
+        completedDigit();
+        unCompletedDigit();
+        console.log(one);
+        console.log(two);
+        console.log(three);
+        console.log(four);
+        console.log(five);
+        console.log(six);
+        console.log(seven);
+        console.log(eight);
+        console.log(nine);
+        console.log("");
+
     }
 }
 
+function completedDigit() {
+    let completed = false;
+    if (digitSelected.id == "1" && one == 9) {
+        completed = true;
+    } else if (digitSelected.id == "2" && two == 9) {
+        completed = true;
+    } else if (digitSelected.id == "3" && three == 9) {
+        completed = true;
+    } else if (digitSelected.id == "4" && four == 9) {
+        completed = true;
+    } else if (digitSelected.id == "5" && five == 9) {
+        completed = true;
+    } else if (digitSelected.id == "6" && six == 9) {
+        completed = true;
+    } else if (digitSelected.id == "7" && seven == 9) {
+        completed = true;
+    } else if (digitSelected.id == "8" && eight == 9) {
+        completed = true;
+    } else if (digitSelected.id == "9" && nine == 9) {
+        completed = true;
+    }
+    if (completed == true) {
+        digitSelected.classList.remove("digits");
+        digitSelected.innerText = "";
+    }
+}
+
+function unCompletedDigit() {
+    if (one != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("1").innerText = "1";
+    }
+    if (two != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("2").innerText = "2";
+    } 
+    if (three != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("3").innerText = "3";
+    }
+    if (four != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("4").innerText = "4";
+    }  
+    if (five != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("5").innerText = "5";
+    }  
+    if (six != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("6").innerText = "6";
+    }  
+    if (seven != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("7").innerText = "7";
+    }  
+    if (eight != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("8").innerText = "8";
+    }  
+    if (nine != 9) {
+        digitSelected.classList.add("digits");
+        document.getElementById("9").innerText = "9";
+    }
+}
+
+function checkToStopPlacingDigits() {
+    switch(digitSelected.id) {
+            case "1":
+                if (one == 9) {
+                    return true;
+                }
+                break;
+            case "2":
+                if (two == 9) {
+                    return true;
+                }
+                break;
+            case "3":
+                if (three == 9) {
+                    return true;
+                }
+                break;
+            case "4":
+                if (four == 9) {
+                    return true;
+                }
+                break;
+            case "5":
+                if (five == 9) {
+                    return true;
+                }
+                break;
+            case "6":
+                if (six == 9) {
+                    return true;
+                }
+                break;
+            case "7":
+                if (seven == 9) {
+                    return true;
+                }
+                break;
+            case "8":
+                if (eight == 9) {
+                    return true;
+                }
+                break;
+            case "9":
+                if (nine == 9) {
+                    return true;
+                }
+                break;
+        }
+}
 
 /* IMPROVEMENTS IN PROGRESS
 - add timer
-- make digit disappear once all 9 are on the board
-- when clicking a number on a boardTile, highlight all of the same numbers on the board
+- when clicking a number on a boardTile, highlight (yellow) all of the same numbers on the board
 - add button to show solution
 - add button to check board
 - add button to check boardTile
@@ -104,7 +339,7 @@ function selectNumber() {
 - add button to clear board
 - using backspace deletes number on board
 - can use keyboard numbers to place down numbers
-- if a number is not supposed to be theer based on current board, make it red
+- if a number is not supposed to be there based on current board, make it red (check 3 blocks)
 */
 
 /* IMPROVEMENTS FINISHED
@@ -112,5 +347,6 @@ function selectNumber() {
 - click boardTile first, then click any digitTile to place a digit
 - allow incorrect placements
 - remove error count
+- make digit disappear once all 9 are on the board
 
 */
