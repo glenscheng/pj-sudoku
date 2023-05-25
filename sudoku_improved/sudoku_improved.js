8ab93447a9b20d065b80ac888fa7602e6cb76340
Glen Cheng <glencheng3000@gmail.com>
2023-05-24 20:23:35 -0400

    deleted one...nine old commented code

--- sudoku_improved/sudoku_improved.js
+++ sudoku_improved/sudoku_improved.js
@@ -101,69 +101,13 @@ function selectDigit() {
         if (oldDigitSelected != null && oldTileSelectedInnerText != "" &&
             tileSelected.innerText != oldTileSelectedInnerText) {
             digitsCount[parseInt(oldTileSelectedInnerText)]--;
-            /*
-            switch(oldTileSelectedInnerText) {
-                case "1":
-                    one--; break;
-                case "2":
-                    two--; break;
-                case "3":
-                    three--; break;
-                case "4":
-                    four--; break;
-                case "5":
-                    five--; break;
-                case "6":
-                    six--; break;
-                case "7":
-                    seven--; break;
-                case "8":
-                    eight--; break;
-                case "9":
-                    nine--; break;
-            }
-            */
         }
         if (/*tileSelected.innerText == "" ||*/ tileSelected.innerText != oldTileSelectedInnerText) {
             digitsCount[parseInt(digitSelected.id)]++;
-            /*
-            switch(digitSelected.id) {
-                case "1":
-                    one++; break;
-                case "2":
-                    two++; break;
-                case "3":
-                    three++; break;
-                case "4":
-                    four++; break;
-                case "5":
-                    five++; break;
-                case "6":
-                    six++; break;
-                case "7":
-                    seven++; break;
-                case "8":
-                    eight++; break;
-                case "9":
-                    nine++; break;
-            }
-            */
         }
         completedDigit();
         unCompletedDigit();
         printDigitsCount();
-        /*
-        console.log(one);
-        console.log(two);
-        console.log(three);
-        console.log(four);
-        console.log(five);
-        console.log(six);
-        console.log(seven);
-        console.log(eight);
-        console.log(nine);
-        console.log("");
-        */
     }
 }
 
@@ -206,49 +150,10 @@ function completedDigit() {
 function unCompletedDigit() {
     for (let i = 1; i <= 9; i++) {
         if (digitsCount[i] != 9) {
-            // digitSelected.classList.add("digits"); // shouldn't this line use i?
             document.getElementById(String(i)).classList.add("digits");
             document.getElementById(String(i)).innerText = String(i);
         }
     }
-    /*
-    if (one != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("1").innerText = "1";
-    }
-    if (two != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("2").innerText = "2";
-    } 
-    if (three != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("3").innerText = "3";
-    }
-    if (four != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("4").innerText = "4";
-    }  
-    if (five != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("5").innerText = "5";
-    }  
-    if (six != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("6").innerText = "6";
-    }  
-    if (seven != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("7").innerText = "7";
-    }  
-    if (eight != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("8").innerText = "8";
-    }  
-    if (nine != 9) {
-        digitSelected.classList.add("digits");
-        document.getElementById("9").innerText = "9";
-    }
-    */
 }
 
 function checkToStopPlacingDigits() {
@@ -256,56 +161,6 @@ function checkToStopPlacingDigits() {
         return true;
     }
     return false;
-    /*
-    switch(digitSelected.id) {
-            case "1":
-                if (one == 9) {
-                    return true;
-                }
-                break;
-            case "2":
-                if (two == 9) {
-                    return true;
-                }
-                break;
-            case "3":
-                if (three == 9) {
-                    return true;
-                }
-                break;
-            case "4":
-                if (four == 9) {
-                    return true;
-                }
-                break;
-            case "5":
-                if (five == 9) {
-                    return true;
-                }
-                break;
-            case "6":
-                if (six == 9) {
-                    return true;
-                }
-                break;
-            case "7":
-                if (seven == 9) {
-                    return true;
-                }
-                break;
-            case "8":
-                if (eight == 9) {
-                    return true;
-                }
-                break;
-            case "9":
-                if (nine == 9) {
-                    return true;
-                }
-                break;
-        }
-    return false;
-    */
 }
 
 function initiateDigitsCount() {
